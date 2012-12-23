99d8d52385bec52b9139740aa1353b557ef05f71
jake <jake@jake-Aspire-M5700.(none)>
2012-12-22 20:58:17 -0500

    Adds setup.sh and prereqs.cfg, which are meant to prepare a fresh EC2 instance for use with the project code

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This script is intended to setup all of the dependancies required for 
+#the rest of this code to run properly. It assumes that it is running on
+#an EC2 instance with the Ubuntu 12.04 image installed.
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo "Running 'apt-get update'..."
+
+apt-get update &> /dev/null
+
+[ $? -ne 0 ] && echo Update failed, exiting script && exit 1
+
+echo "Installing prerequisite packages from prereqs.cfg"
+
+while read -r pkg
+do
+    echo "  Installing $pkg"
+    apt-get install --force-yes "$pkg" &> /dev/null
+    [ $? -eq 0 ] && { continue; }
+done < prereqs.cfg
+
